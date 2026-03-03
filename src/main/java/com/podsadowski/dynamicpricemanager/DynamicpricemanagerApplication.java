7fdaba5b74b205c7248a4037ef9fa9048f6ca94e
Audixable <podsadowskijakub@gmail.com>
2026-03-03 13:14:56 +0100

    init

---
+++ src/main/java/com/podsadowski/dynamicpricemanager/DynamicpricemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.podsadowski.dynamicpricemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicpricemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicpricemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/podsadowski/dynamicpricemanager/DynamicpricemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podsadowski.dynamicpricemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicpricemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
